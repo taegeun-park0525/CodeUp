353908fdb45e69009588a35ec67c58b0fc89f3d9
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-09 21:36:37 +0900

    CodeUp 2022.10.08 [60/100] 비트논리연산자에 대해 알게됨
    비트논리연산자에 대해 알게되었다.
    비트 이동연산자도 같이 다시 정리함.
    x << y : 정수 x의 각 비트를 y만큼 왼쪽으로 이동 (빈자리는 0으로채워짐)
    x >> y : 정수 x의 각 비트를 y만큼 오른쪽으로 이동(빈자리는 정수 a의 최상위 부호비트와 같은 값으로 채워짐)
    x  >>> y : 정수 x의 각 비트를 yt만큼 오른쪽으로 이동 (빈자리는 0으로 채워짐)
    비트 논리연산자
    &   논리(AND)  : 두 비트 모두 1일 경우에만 연산 결과가 1
    | 논리(OR) : 두 비트 중 하나만 1일 경우에만 연산결과가 1
    ^ 논리(XOR) 두 비트중 하나는 1이고 다른 하나가0일경우에만 연산결과가 1
    ~ 논리(NOT) 비트반전(보수)

---
+++ src/Problem100/Q1052.java
@@ -0,0 +1,15 @@
+package Problem100;
+
+import java.util.Scanner;
+
+public class Q1052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        if(num1 == 0) {
+            System.out.println(1);
+        }else {
+            System.out.println(0);
+        }
+    }
+}
---
+++ src/Problem100/Q1059.java
@@ -0,0 +1,11 @@
+package Problem100;
+
+import java.util.Scanner;
+
+public class Q1059 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.println(~num1);
+    }
+}
