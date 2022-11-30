48bca5e7a861d1a2e37dc441a941783914032d4e
taegeun <djqtek0525@naver.com>
2022-11-30 16:18:40 +0900

    [add] up

---
+++ src/Problem100/Q1081.java
@@ -0,0 +1,17 @@
+package Problem100;
+
+import java.util.Scanner;
+
+public class Q1081 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.printf("%d %d\n",i,j);
+            }
+        }
+    }
+}
---
+++ src/Problem100/Q1082.java
@@ -0,0 +1,15 @@
+package Problem100;
+
+import java.util.Scanner;
+
+public class Q1082 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(16);
+
+        for(int i=1; i<=16; i++) {
+                System.out.printf("%X*%X=%X\n",n,i,n*i);
+
+        }
+    }
+}
