375c05e37ef2f8ebfc4fd83f2fedf518e5d7afe3
taegeun <djqtek0525@naver.com>
2022-11-30 16:44:55 +0900

    [add] up

--- src/Problem100/Q1082.java
+++ src/Problem100/Q1082.java
@@ -7,9 +7,8 @@ public class Q1082 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(16);
 
-        for(int i=1; i<=16; i++) {
+        for(int i=1; i<16; i++) {
                 System.out.printf("%X*%X=%X\n",n,i,n*i);
-
         }
     }
 }
---
+++ src/Problem100/Q1083.java
@@ -0,0 +1,17 @@
+package Problem100;
+
+import java.util.Scanner;
+
+public class Q1083 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                System.out.print("X");
+            } else System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
